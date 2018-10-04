7e59b75925983cb2d082cb4b7e62608b96113bc5
MaryKattyVO <mkvicunaore@gmail.com>
2018-10-04 18:41:30 -0500

    avance

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-//-------------------- DOM ----------------------------------------------
+// -------------------- DOM ----------------------------------------------
 const nameTxt = document.getElementById('inputName');
 const lastName = document.getElementById('inputLastName');
 const identification = document.getElementById('inputIdentification');
@@ -6,8 +6,8 @@ const company = document.getElementById('inputCompany');
 const anfitrion = document.getElementById('inputAnfitrion');
 const menssage = document.getElementById('inputMenssage');
 const btnEnviar = document.getElementById('btnEnviar');
-const date = new Date().getDate() + 'day ' + new Date().getMonth() + 'month ' + new Date().getFullYear() + 'year ';
-const hour = new Date().getHours() + 'h ' + new Date().getMinutes() + 'min ' + new Date().getSeconds() + 'sec';
+const date = new Date().getDate() + '/' + new Date().getMonth() + '/' + new Date().getFullYear();
+const hour = new Date().getHours() + ':' + new Date().getMinutes()+horas;
 
 btnEnviar.addEventListener('click', () => {
     const name = nameTxt.value;
@@ -16,5 +16,26 @@ btnEnviar.addEventListener('click', () => {
     const empresa = company.value;
     const anfitrions = anfitrion.value;
     const mensaje = menssage.value;
-    alert(name);
+    const obj={
+        name,
+        apellido,
+        identificacion,
+        empresa,
+        anfitrions,
+        mensaje,
+        date,
+        hour
+        
+    }
+    firebase.database().ref('visitors/one').set(obj);
 })
+
+// //get: obtener elements
+// const preObject = document.getElementById('object');
+
+// //Crear referencias
+// const dbRefObject = firebase.database().ref().child('object');
+
+// //Sincronizar object changes(cambio)
+
+// dbRefObject.on('value', snap => console.log(snap.val()));
