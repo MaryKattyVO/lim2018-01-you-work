fb5042a32bc9f3883a15e7ccc8df221906f6c421
MaryKattyVO <mkvicunaore@gmail.com>
2018-10-05 17:03:03 -0500

    avance

--- js/data.js
+++ js/data.js
@@ -7,56 +7,63 @@ const anfitrion = document.getElementById('inputAnfitrion');
 const menssage = document.getElementById('inputMenssage');
 const btnEnviar = document.getElementById('btnEnviar');
 const date = new Date().getDate() + '/' + new Date().getMonth() + '/' + new Date().getFullYear();
-const hour = new Date().getHours() + ':' + new Date().getMinutes();
+const hour = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
 
 btnEnviar.addEventListener('click', () => {
-    const name = nameTxt.value;
-    const apellido = lastName.value;
-    const identificacion = identification.value;
-    const empresa = company.value;
-    const anfitrions = anfitrion.value;
-    const mensaje = menssage.value;
-    const obj={
-        name,
-        apellido,
-        identificacion,
-        empresa,
-        anfitrions,
-        mensaje,
-        date,
-        hour
-        
-    }
-    firebase.database().ref('visitors/one').set(obj);
+  const name = nameTxt.value;
+  const apellido = lastName.value;
+  const identificacion = identification.value;
+  const empresa = company.value;
+  const anfitrions = anfitrion.value;
+  const mensaje = menssage.value;
+  const obj = {
+    name,
+    apellido,
+    identificacion,
+    empresa,
+    anfitrions,
+    mensaje,
+    date,
+    hour,
+    photo: dataURL
+
+  }
+  firebase.database().ref().child('visitors').push(obj);
+  
 })
 
-//----------------CAPTURAR IMAGEN
+//---------------- CAPTURAR IMAGEN --------------------------------
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
-var start = document.querySelector('#start'),
-    capture = document.querySelector('#capture'),
-    canvas = document.querySelector('canvas'),
-    ctx = canvas.getContext('2d'),
-    video = document.querySelector('video');
+const btnStarts = document.querySelector('#btnStarts');
+const btnCapture = document.querySelector('#btnCapture');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const video = document.querySelector('video');
 
-start.addEventListener('click', function () {
+btnStarts.addEventListener('click', function () {
 
-    navigator.getUserMedia({
-      video: true
-    }, function (stream) {
-      var src = window.URL.createObjectURL(stream);
-      video.src = src;
-    }, function (e) {
-      console.log(e);
-    });
+  navigator.getUserMedia({
+    video: true
+  }, function (stream) {
+    var src = window.URL.createObjectURL(stream);
+    video.src = src;
+  }, function (e) {
+    console.log(e);
+  });
 
-    capture.addEventListener('click', function () {
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    }, false);
+  btnCapture.addEventListener('click', function () {
+    var canvas = document.getElementById('canvas');
+    var dataURL = canvas.toDataURL();
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    console.log(dataURL);
+  }, false);
 
 }, false);
+
+
 // //get: obtener elements
 // const preObject = document.getElementById('object');
 
