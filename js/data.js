ba2329b05404a62833f6ea7e2f2c53c12bb276a1
MaryKattyVO <mkvicunaore@gmail.com>
2018-10-05 18:32:50 -0500

    avance

--- js/data.js
+++ js/data.js
@@ -3,7 +3,6 @@ const nameTxt = document.getElementById('inputName');
 const lastName = document.getElementById('inputLastName');
 const identification = document.getElementById('inputIdentification');
 const company = document.getElementById('inputCompany');
-const anfitrion = document.getElementById('inputAnfitrion');
 const menssage = document.getElementById('inputMenssage');
 const btnEnviar = document.getElementById('btnEnviar');
 const date = new Date().getDate() + '/' + new Date().getMonth() + '/' + new Date().getFullYear();
@@ -14,19 +13,18 @@ btnEnviar.addEventListener('click', () => {
   const apellido = lastName.value;
   const identificacion = identification.value;
   const empresa = company.value;
-  const anfitrions = anfitrion.value;
+  const selector = document.getElementById('selector').value;
   const mensaje = menssage.value;
   const obj = {
     name,
     apellido,
     identificacion,
     empresa,
-    anfitrions,
+    selector,
     mensaje,
     date,
-    hour,
-    photo: dataURL
-
+    hour
+    // photo: dataURL
   }
   firebase.database().ref().child('visitors').push(obj);
 
@@ -34,6 +32,7 @@ btnEnviar.addEventListener('click', () => {
   ref.once('value', (data) => {
     data.forEach(visitor => {
       const visitorDate = visitor.val();
+      console.log(visitorDate)
       sendMandrill(visitorDate);
     })
   })
