a1766e9018edb401b58d688217c98078bfdbf1a2
MaryKattyVO <mkvicunaore@gmail.com>
2018-10-05 12:18:35 -0500

    avance

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@ const anfitrion = document.getElementById('inputAnfitrion');
 const menssage = document.getElementById('inputMenssage');
 const btnEnviar = document.getElementById('btnEnviar');
 const date = new Date().getDate() + '/' + new Date().getMonth() + '/' + new Date().getFullYear();
-const hour = new Date().getHours() + ':' + new Date().getMinutes()+horas;
+const hour = new Date().getHours() + ':' + new Date().getMinutes();
 
 btnEnviar.addEventListener('click', () => {
     const name = nameTxt.value;
@@ -30,6 +30,33 @@ btnEnviar.addEventListener('click', () => {
     firebase.database().ref('visitors/one').set(obj);
 })
 
+//----------------CAPTURAR IMAGEN
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+
+var start = document.querySelector('#start'),
+    capture = document.querySelector('#capture'),
+    canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d'),
+    video = document.querySelector('video');
+
+start.addEventListener('click', function () {
+
+    navigator.getUserMedia({
+      video: true
+    }, function (stream) {
+      var src = window.URL.createObjectURL(stream);
+      video.src = src;
+    }, function (e) {
+      console.log(e);
+    });
+
+    capture.addEventListener('click', function () {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    }, false);
+
+}, false);
 // //get: obtener elements
 // const preObject = document.getElementById('object');
 
